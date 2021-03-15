b889a7aa83fe28fa4152807261f9dfbc32ba2330
RoyalByte <adrian.schiel.2904@gmail.com>
2021-03-15 12:14:30 +0100

    JavaDoc ArenaHandler.java

--- src/de/byteevolve/gungame/arena/ArenaHandler.java
+++ src/de/byteevolve/gungame/arena/ArenaHandler.java
@@ -11,21 +11,40 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * ArenaHandler dient zum verwalten aller Maps im GunGame System.
+ * @author RoyalByte | Adrian Schiel
+ * @since 0.1
+ * @version 1.0
+ * @see Arena
+ */
 public class ArenaHandler {
 
     private List<Arena> arenas;
     private Map<Player, Arena> arenaPlayerCreate;
 
+    /**
+     * Konstrukter für die Klasse {@link ArenaHandler}
+     */
     public ArenaHandler() {
         this.arenas = new ArrayList<>();
         this.arenaPlayerCreate = new HashMap<>();
         loadArenas();
     }
 
+    /**
+     * Gibt die Arena Liste wieder.
+     * In dieser werden alle Arenen des Systems registriert
+     * @return Liste aller Arenen
+     */
     public List<Arena> getArenas() {
         return arenas;
     }
 
+    /**
+     * Lädt alle Arenen aus der SQL Datenbank aus, und schreibt sie in die Arena Liste
+     * @return Wahrheitswert, ob das laden geklappt hat
+     */
     public boolean loadArenas() {
         this.arenas.clear();
         ResultSet resultSet = GunGame.getInstance().getMySQL().getResult("SELECT * FROM gg_arena;");
@@ -47,6 +66,12 @@ public class ArenaHandler {
         }
     }
 
+
+    /**
+     * Gibt eine Arena anhand ihres Namens in der Arena Liste wieder {@link Arena}
+     * @param name Der abzufragende Name
+     * @return Gesuchte Arena
+     */
     public Arena getArenaByName(String name){
         for(Arena arena : arenas){
             if(arena.getName().equalsIgnoreCase(name)) return arena;
@@ -54,6 +79,11 @@ public class ArenaHandler {
         return null;
     }
 
+    /**
+     * Fragt ab ob es eine Arena mit einem bestimmten Namen gibt, welcher ein Spieler erstellt.
+     * @param name Der abzufragende Name
+     * @return Wahrheitswert, ob diese Arena gerade von einem Spieler erstellt wird.
+     */
     public boolean existArenaPlayerCreateByName(String name) {
         for (Player player : this.arenaPlayerCreate.keySet()){
             if(this.arenaPlayerCreate.get(player).getName().equalsIgnoreCase(name)) return true;
@@ -61,11 +91,19 @@ public class ArenaHandler {
         return false;
     }
 
-
+    /**
+     * Gibt die Map ArenaPlayerCreate wieder
+     * @return Map<Player, Arena>
+     */
     public Map<Player, Arena> getArenaPlayerCreate() {
         return arenaPlayerCreate;
     }
 
+    /**
+     * Fragt ab ob es eine Arena mit einem bestimmten Namen gibt.
+     * @param name Der abzufragende Name
+     * @return Wahrheitswert, ob diese Arena vorhanden ist.
+     */
     public boolean existArenaByName(String name){
         for(Arena arena : this.arenas){
             if(arena.getName().equalsIgnoreCase(name)) return true;
