335c44e4dcc08333547511aa07c7d6c11c626289
RoyalByte <adrian.schiel.2904@gmail.com>
2021-03-15 12:19:26 +0100

    JavaDoc ArenaState.java

--- src/de/byteevolve/gungame/arena/ArenaState.java
+++ src/de/byteevolve/gungame/arena/ArenaState.java
@@ -1,16 +1,36 @@
 package de.byteevolve.gungame.arena;
 
+
+/**
+ * Available ArenaStates
+ * {@link #FINISHED}
+ * {@link #UNFINISHED}
+ */
 public enum ArenaState {
 
+    /**
+     * FINISHED
+     */
     FINISHED("§a"),
+
+    /**
+     * UNFINISHED
+     */
     UNFINISHED("§c");
 
     private final String prefix;
 
+    /**
+     * @return Prefix from ArenaState
+     */
     public String getPrefix() {
         return prefix;
     }
 
+    /**
+     * Construkter from ArenaStates
+     * @param prefix from ArenaState
+     */
     ArenaState(String prefix){
         this.prefix = prefix;
     }
