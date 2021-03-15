2cdf608799f8a6eedd555d0ef99455b3903400f9
RoyalByte <adrian.schiel.2904@gmail.com>
2021-03-15 12:21:24 +0100

    JavaDoc ArenaTeamState.java

--- src/de/byteevolve/gungame/arena/ArenaTeamState.java
+++ src/de/byteevolve/gungame/arena/ArenaTeamState.java
@@ -1,16 +1,36 @@
 package de.byteevolve.gungame.arena;
 
+
+/**
+ * Available ArenaTeamStates
+ * {@link #ALLOWED}
+ * {@link #DISALLOWED}
+ */
 public enum ArenaTeamState {
 
+    /**
+     * ALLOWED
+     */
     ALLOWED("§aTeams erlaubt"),
+
+    /**
+     * DISALLOWED
+     */
     DISALLOWED("§cTeams verboten");
 
     private String prefix;
 
+    /**
+     * @return Prefix from ArenaTeamState
+     */
     public String getPrefix() {
         return prefix;
     }
 
+    /**
+     * Construkter from ArenaTeamState
+     * @param prefix from ArenaTeamState
+     */
     ArenaTeamState(String prefix) {
         this.prefix = prefix;
     }
