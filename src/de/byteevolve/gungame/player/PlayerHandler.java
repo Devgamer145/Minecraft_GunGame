c18d71425050afe79c6bd49cb1f3a3ce6f5e0670
RoyalByte <adrian.schiel.2904@gmail.com>
2021-05-10 23:29:07 +0200

    addet multiple Version Scoreboards

--- src/de/byteevolve/gungame/player/PlayerHandler.java
+++ src/de/byteevolve/gungame/player/PlayerHandler.java
@@ -4,19 +4,15 @@ import de.byteevolve.gungame.GunGame;
 import de.byteevolve.gungame.arena.Arena;
 import de.byteevolve.gungame.arena.ArenaState;
 import de.byteevolve.gungame.arena.ArenaTeamState;
-import de.byteevolve.gungame.configuration.config.ConfigEntries;
 import de.byteevolve.gungame.itembuilder.ItemBuilder;
-import net.minecraft.server.v1_8_R3.*;
+import de.byteevolve.gungame.player.scoreboard.*;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Sound;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class PlayerHandler {
 
@@ -31,54 +27,54 @@ public class PlayerHandler {
     }
 
     public void sendActionBar(String text) {
-        PacketPlayOutChat packet = new PacketPlayOutChat(IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + text + "\"}"), (byte)2);
-        sendPacket(packet);
+        //PacketPlayOutChat packet = new PacketPlayOutChat(IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + text + "\"}"), (byte)2);
+        //sendPacket(packet);
     }
 
     public void sendScoreBoard() {
-        Scoreboard sb = new Scoreboard();
-        ScoreboardObjective obj = sb.registerObjective(ConfigEntries.SCOREBOARDNAME.getAsString(), IScoreboardCriteria.b);
-        PacketPlayOutScoreboardObjective createpacket = new PacketPlayOutScoreboardObjective(obj, 0);
-        PacketPlayOutScoreboardDisplayObjective display = new PacketPlayOutScoreboardDisplayObjective(1, obj);
-        obj.setDisplayName("GUNGAME");
+        GGScoreboard ggScoreboard = null;
+        String version = "N/A";
+        try{
+            version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
 
-        PlayerStats playerStats = new PlayerStats(this.player.getUniqueId().toString());
+            switch (version) {
+                case "v1_8_R3":
+                    ggScoreboard = new v1_8_R3_Scoreboard();
+                    break;
+                case "v1_9_R2":
+                    ggScoreboard = new v1_9_R2_Scoreboard();
+                    break;
+                case "v1_10_R1":
+                    ggScoreboard = new v1_10_R1_Scoreboard();
+                    break;
+                case "v1_11_R1":
+                    ggScoreboard = new v1_11_R1_Scoreboard();
+                    break;
+                case "v1_12_R1":
+                    ggScoreboard = new v1_12_R1_Scoreboard();
+                    break;
+                case "v1_13_R2":
+                    ggScoreboard = new v1_13_R2_Scoreboard();
+                    break;
+                case "v1_14_R1":
+                    ggScoreboard = new v1_14_R1_GunGame();
+                    break;
+                case "v1_15_R1":
+                    ggScoreboard = new v1_15_R1_Scoreboard();
+                    break;
+                case "v1_16_R3":
+                    ggScoreboard = new v1_16_R3_Scoreboard();
+                    break;
+            }
 
-        String map;
-        if(GunGame.getInstance().getGameHandler().getCurrent() != null) {
-            map = GunGame.getInstance().getGameHandler().getCurrent().getDisplayname().replaceAll("&", "§");
-        } else map = "-";
+            ggScoreboard.sendScoreboard(this.player);
 
-        PacketPlayOutScoreboardObjective removePacket = new PacketPlayOutScoreboardObjective(obj, 1);
-        sendPacket(removePacket);
-        sendPacket(createpacket);
-        sendPacket(display);
-
-        int i = 16;
-        for(String line : ConfigEntries.SCOREBOARD.getAsString().split("\n")){
-            line = line.replaceAll("%MAP%", map);
-            line = line.replaceAll("%RECORD%", String.valueOf(playerStats.get(PlayerStatsType.HIGHSCORE)));
-            line = line.replaceAll("%KILLS%", String.valueOf(playerStats.get(PlayerStatsType.KILLS)));
-            line = line.replaceAll("%DEATHS%", String.valueOf(playerStats.get(PlayerStatsType.DEATHS)));
-            line = line.replaceAll("%RANK%", String.valueOf(playerStats.getRank()));
-            line = line.replaceAll("%KD%", String.valueOf(playerStats.getKD()));
-
-
-            ScoreboardScore score = new ScoreboardScore(sb, obj, line);
-            score.setScore(i);
-
-            PacketPlayOutScoreboardScore packet= new PacketPlayOutScoreboardScore(score);
-            sendPacket(packet);
-
-            i--;
+        }catch (ArrayIndexOutOfBoundsException ex){
+            ex.printStackTrace();
         }
-
-
     }
 
-    public void sendPacket(Packet<?> packet) {
-        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
-    }
+
 
     public void openArenaEditMainInv(Arena arena){
         Inventory inventory = Bukkit.createInventory(null, 3*9, "§8Arena: §a" + arena.getName());
