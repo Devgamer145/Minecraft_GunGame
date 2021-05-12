0223ae07ffe833d228912fe853d176024bebd2b6
RoyalByte <adrian.schiel.2904@gmail.com>
2021-05-12 21:30:31 +0200

    Removed Timer at ArenaEditInv

--- src/de/byteevolve/gungame/player/PlayerHandler.java
+++ src/de/byteevolve/gungame/player/PlayerHandler.java
@@ -120,67 +120,18 @@ public class PlayerHandler {
         Inventory inventory = Bukkit.createInventory(null, 3*9, "§8Arena: §a" + arena.getName());
         player.openInventory(inventory);
 
-        new BukkitRunnable() {
-            int i = 0;
-            @Override
-            public void run() {
-                if(i<8){
-                    player.playSound(player.getLocation(), Sound.GLASS, 100, 100);
-                }
-                switch (i){
-                    case 0:
-                        inventory.setItem(0, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        inventory.setItem(9, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        inventory.setItem(18, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        break;
-                    case 1:
-                        inventory.setItem(1, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        inventory.setItem(10, new ItemBuilder(Material.MAGMA_CREAM, 1).setName("§7Spawn setzen").build());
-                        inventory.setItem(19, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        break;
-                    case 2:
-                        inventory.setItem(2, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        inventory.setItem(11, new ItemBuilder(Material.BARRIER, 1).setName("§aObere-SpawnProt setzen").build());
-                        inventory.setItem(20, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        break;
-                    case 3:
-                        inventory.setItem(3, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        inventory.setItem(12, new ItemBuilder(Material.BARRIER, 1).setName("§aUntere-SpawnProt setzen").build());
-                        inventory.setItem(21, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        break;
-                    case 4:
-                        inventory.setItem(4, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        inventory.setItem(13, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        inventory.setItem(22, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        break;
-                    case 5:
-                        if(arena.getArenaTeamState() == null) arena.setArenaTeamState(ArenaTeamState.ALLOWED);
-                        inventory.setItem(5, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        inventory.setItem(14, new ItemBuilder(Material.ARMOR_STAND, 1).setName("§aTeamsState §7setzen").addLore("§7State: §a" + arena.getArenaTeamState().toString()).build());
-                        inventory.setItem(23, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        break;
-                    case 6:
-                        inventory.setItem(6, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        ArenaState arenaState = arena.getArenaStateFromInt(arena.getFinished());
-                        inventory.setItem(15, new ItemBuilder(Material.BEACON, 1).setSubId(7).setName("§aStatus").addLore("§7Status: " + arenaState.getPrefix() + arenaState.toString()).build());
-                        inventory.setItem(24, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        break;
-                    case 7:
-                        inventory.setItem(7, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        inventory.setItem(16, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        inventory.setItem(25, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        break;
-                    case 8:
-                        inventory.setItem(8, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        inventory.setItem(17, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
-                        inventory.setItem(26, new ItemBuilder(Material.INK_SACK, 1).setSubId(10).setName("§aBeenden").build());
-                        player.playSound(player.getLocation(), Sound.LEVEL_UP, 10, 10);
-                        cancel();
-                        break;
-                }
+        for (int i = 0; i < inventory.getSize() ; i++) {
+            inventory.setItem(i, new ItemBuilder(Material.STAINED_GLASS_PANE, 1).setSubId(7).setName(" ").build());
+        }
+        inventory.setItem(10, new ItemBuilder(Material.MAGMA_CREAM, 1).setName("§7Spawn setzen").build());
+        inventory.setItem(11, new ItemBuilder(Material.BARRIER, 1).setName("§aObere-SpawnProt setzen").build());
+        inventory.setItem(12, new ItemBuilder(Material.BARRIER, 1).setName("§aUntere-SpawnProt setzen").build());
 
-                i++;
-            }
-        }.runTaskTimerAsynchronously(GunGame.getInstance(), 0, 1);
+        if(arena.getArenaTeamState() == null) arena.setArenaTeamState(ArenaTeamState.ALLOWED);
+        inventory.setItem(14, new ItemBuilder(Material.ARMOR_STAND, 1).setName("§aTeamsState §7setzen").addLore("§7State: §a" + arena.getArenaTeamState().toString()).build());
+        ArenaState arenaState = arena.getArenaStateFromInt(arena.getFinished());
+        inventory.setItem(15, new ItemBuilder(Material.BEACON, 1).setSubId(7).setName("§aStatus").addLore("§7Status: " + arenaState.getPrefix() + arenaState.toString()).build());
+        inventory.setItem(26, new ItemBuilder(Material.INK_SACK, 1).setSubId(10).setName("§aBeenden").build());
+
     }
 }
