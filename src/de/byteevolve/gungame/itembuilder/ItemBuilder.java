a018cabd334a2f73bbd3503ca941aebfca393a1b
RoyalByte <adrian.schiel.2904@gmail.com>
2021-05-12 21:29:02 +0200

    Addet Unbreakable feature to ItemBuilder

--- src/de/byteevolve/gungame/itembuilder/ItemBuilder.java
+++ src/de/byteevolve/gungame/itembuilder/ItemBuilder.java
@@ -1,7 +1,11 @@
 package de.byteevolve.gungame.itembuilder;
 
+import de.byteevolve.gungame.itembuilder.unbreakable.*;
+import de.byteevolve.gungame.player.scoreboard.*;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -17,24 +21,74 @@ public class ItemBuilder {
     private List<String> lore;
     private Enchantment enchantment;
     private int level;
+    private boolean unbreakable;
 
     public ItemBuilder(Material material, int count) {
         this.material = material;
         this.count = count;
         this.lore = new ArrayList<>();
+        this.unbreakable = false;
     }
 
     public ItemStack build() {
         ItemStack itemStack = new ItemStack(this.material, this.count, (short) this.subid);
-        ItemMeta itemMeta = itemStack.getItemMeta();
-        itemMeta.setLore(this.lore);
-        itemMeta.setDisplayName(this.name);
-        if(enchantment != null){
-            itemMeta.addEnchant(enchantment,level,true);
+
+
+        if(this.unbreakable) {
+            Unbreakable unbreakable = null;
+
+            String version = "N/A";
+            try {
+                version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+
+                switch (version) {
+                    case "v1_8_R3":
+                        unbreakable = new v1_8_R3_Unbreakable();
+                        break;
+                    case "v1_9_R2":
+                        unbreakable = new v1_9_R2_Unbreakable();
+                        break;
+                    case "v1_10_R1":
+                        unbreakable = new v1_10_R1_Unbreakable();
+                        break;
+                    case "v1_11_R1":
+                        unbreakable = new v1_11_R1_Unbreakable();
+                        break;
+                    case "v1_12_R1":
+                        unbreakable = new v1_12_R1_Unbreakable();
+                        break;
+                    case "v1_13_R2":
+                        unbreakable = new v1_13_R2_Unbreakable();
+                        break;
+                    case "v1_14_R1":
+                        unbreakable = new v1_14_R1_Unbreakable();
+                        break;
+                    case "v1_15_R1":
+                        unbreakable = new v1_15_R1_Unbreakable();
+                        break;
+                    case "v1_16_R3":
+                        unbreakable = new v1_16_R3_Unbreakable();
+                        break;
+                }
+
+                itemStack = unbreakable.makeUnbreakable(itemStack);
+
+            } catch (ArrayIndexOutOfBoundsException ex) {
+                ex.printStackTrace();
+            }
+
         }
-        itemStack.setItemMeta(itemMeta);
-        return itemStack;
-    }
+
+            ItemMeta itemMeta = itemStack.getItemMeta();
+            itemMeta.setLore(this.lore);
+            itemMeta.setDisplayName(this.name);
+            if (enchantment != null) {
+                itemMeta.addEnchant(enchantment, level, true);
+            }
+            itemStack.setItemMeta(itemMeta);
+            return itemStack;
+        }
+
 
     public ItemBuilder setMaterial(Material material) {
         this.material = material;
@@ -43,6 +97,11 @@ public class ItemBuilder {
     public ItemBuilder addEnchant(Enchantment enchantment, int level) {
         this.enchantment = enchantment;
         this.level = level;
+        return this;
+    }
+
+    public ItemBuilder setUnbreakable(boolean unbreakable) {
+        this.unbreakable = unbreakable;
         return this;
     }
 
