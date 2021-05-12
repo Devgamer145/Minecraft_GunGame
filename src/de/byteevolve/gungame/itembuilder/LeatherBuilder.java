caa26dd65d575eaf0accf4cf748fadcff62f84a4
RoyalByte <adrian.schiel.2904@gmail.com>
2021-05-12 21:29:12 +0200

    Addet Unbreakable feature to LeatherBuilder

--- src/de/byteevolve/gungame/itembuilder/LeatherBuilder.java
+++ src/de/byteevolve/gungame/itembuilder/LeatherBuilder.java
@@ -1,5 +1,7 @@
 package de.byteevolve.gungame.itembuilder;
 
+import de.byteevolve.gungame.itembuilder.unbreakable.*;
+import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
@@ -18,15 +20,63 @@ public class LeatherBuilder {
     private int count;
     private Enchantment enchantment;
     private int level;
+    private boolean unbreakable;
 
     public LeatherBuilder(Material material, String name) {
         this.material = material;
         this.name = name;
+        this.unbreakable = false;
     }
 
 
     public ItemStack build() {
         ItemStack itemStack = new ItemStack(this.material, this.count);
+
+        if(this.unbreakable) {
+            Unbreakable unbreakable = null;
+
+            String version = "N/A";
+            try {
+                version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+
+                switch (version) {
+                    case "v1_8_R3":
+                        unbreakable = new v1_8_R3_Unbreakable();
+                        break;
+                    case "v1_9_R2":
+                        unbreakable = new v1_9_R2_Unbreakable();
+                        break;
+                    case "v1_10_R1":
+                        unbreakable = new v1_10_R1_Unbreakable();
+                        break;
+                    case "v1_11_R1":
+                        unbreakable = new v1_11_R1_Unbreakable();
+                        break;
+                    case "v1_12_R1":
+                        unbreakable = new v1_12_R1_Unbreakable();
+                        break;
+                    case "v1_13_R2":
+                        unbreakable = new v1_13_R2_Unbreakable();
+                        break;
+                    case "v1_14_R1":
+                        unbreakable = new v1_14_R1_Unbreakable();
+                        break;
+                    case "v1_15_R1":
+                        unbreakable = new v1_15_R1_Unbreakable();
+                        break;
+                    case "v1_16_R3":
+                        unbreakable = new v1_16_R3_Unbreakable();
+                        break;
+                }
+
+                itemStack = unbreakable.makeUnbreakable(itemStack);
+
+            } catch (ArrayIndexOutOfBoundsException ex) {
+                ex.printStackTrace();
+            }
+
+        }
+
         LeatherArmorMeta leatherArmorMeta = (LeatherArmorMeta) itemStack.getItemMeta();
         leatherArmorMeta.setColor(this.color);
         leatherArmorMeta.setDisplayName(this.name);
@@ -42,7 +92,10 @@ public class LeatherBuilder {
         this.level = level;
         return this;
     }
-
+    public LeatherBuilder setUnbreakable(boolean unbreakable) {
+        this.unbreakable = unbreakable;
+        return this;
+    }
     public LeatherBuilder setMaterial(Material material) {
         this.material = material;
         return this;
