07af1aaa71855fb02109e292f94ea198fc2a508d
RoyalByte <adrian.schiel.2904@gmail.com>
2021-05-14 13:06:07 +0200

    Addet 1.12 Respawn

---
+++ src/de/byteevolve/gungame/player/respawn/v1_12_R1_Respawn.java
@@ -0,0 +1,25 @@
+package de.byteevolve.gungame.player.respawn;
+
+import de.byteevolve.gungame.GunGame;
+import net.minecraft.server.v1_12_R1.PacketPlayInClientCommand;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class v1_12_R1_Respawn implements GGRespawn{
+
+    @Override
+    public void respawn(Player player) {
+        final CraftPlayer craftPlayer = (CraftPlayer) player;
+        GunGame.getInstance().getServer().getScheduler()
+                .scheduleSyncDelayedTask(GunGame.getInstance(), new Runnable() {
+                    public void run() {
+                        if (player.isDead()) {
+                            craftPlayer.getHandle().playerConnection
+                                    .a(new PacketPlayInClientCommand(
+                                            PacketPlayInClientCommand.EnumClientCommand.PERFORM_RESPAWN));
+                        }
+                    }
+                });
+    }
+
+}
